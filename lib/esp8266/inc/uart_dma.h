191a7cecae571e7bc14a8d6d17810053244f8f22
wpirkl <werner.pirkl@gmail.com>
2015-09-29 16:28:06 +0200

    move documentation to header file

--- lib/esp8266/inc/uart_dma.h
+++ lib/esp8266/inc/uart_dma.h
@@ -4,17 +4,126 @@
 #include <stdbool.h>
 #include <stdint.h>
 
+/*!
+    Initialize the USART using DMA
+*/
+void usart_dma_open(void);
 
-void   usart_dma_open(void);
+
+/*!
+    Get the number of received bytes
+
+    \return the number of entries
+*/
 size_t usart_dma_rx_num(void);
-size_t usart_dma_read_until(uint8_t * inBuffer, size_t inMaxNumBytes, uint8_t inCharacter);
-size_t usart_dma_read_until_str(uint8_t * inBuffer, size_t inMaxNumBytes, size_t inStartOffset, uint8_t * inString, size_t inStringLength);
-bool   usart_dma_peek(uint8_t * inString, size_t inStringLength);
-void   usart_dma_rx_skip(size_t inNumberOfCharacters);
+
+
+/*!
+    Get a number of bytes from the rx buffer
+
+    This function is nonblocking. It will return 0 if nothing's received
+
+    \param[in]  inBuffer
+    \param[in]  inMaxNumBytes
+
+    \retval     The number of bytes read
+*/
 size_t usart_dma_read(uint8_t * inBuffer, size_t inMaxNumBytes);
+
+
+/*!
+    Get a number of bytes from the rx buffer until a certain character or end of buffer
+
+    This function is nonblocking. It will return 0 if nothing's received
+
+    \param[in]  inBuffer
+    \param[in]  inMaxNumBytes   
+    \param[in]  inCharacter     The character to stop reading
+
+    \retval     The number of bytes read
+*/
+size_t usart_dma_read_until(uint8_t * inBuffer, size_t inMaxNumBytes, uint8_t inCharacter);
+
+
+/*!
+    Get a number of bytes from the rx buffer until a certain character or end of buffer
+
+    This function is nonblocking. It will return 0 if nothing's received
+
+    \param[in]  inBuffer
+    \param[in]  inMaxNumBytes   
+    \param[in]  inStartOffset   Initial offset into the buffer
+    \param[in]  inString        The character sequence to compare
+    \param[in]  inStringLength  The length of the character sequence to match
+
+    \retval     The number of bytes read
+*/
+size_t usart_dma_read_until_str(uint8_t * inBuffer, size_t inMaxNumBytes, size_t inStartOffset, uint8_t * inString, size_t inStringLength);
+
+
+/*!
+    Check if the buffer starts with a certain pattern
+
+    \param[in]  inString        The character sequence to compare
+    \param[in]  inStringLength  The length of the character sequence to match
+
+    \retval     true: if the buffer starts with the string, false if it doesn't
+*/
+bool usart_dma_peek(uint8_t * inString, size_t inStringLength);
+
+
+/*!
+    Check if the buffer ends with a certain pattern
+
+    \param[in]  inString        The character sequence to compare
+    \param[in]  inStringLength  The length of the character sequence to match
+
+    \retval     true: if the buffer ends with the string, false if it doesn't
+*/
+bool usart_dma_peek_end(uint8_t * inString, size_t inStringLength);
+
+
+/*!
+    Check if the buffer contains a certain pattern
+
+    \param[in]  inString        The character sequence to compare
+    \param[in]  inStringLength  The length of the character sequence to match
+
+    \retval     0   string not found
+    \retval     >0  Length to retreive including inString
+*/
+size_t usart_dma_match(uint8_t * inString, size_t inStringLength);
+
+
+/*!
+    Skip a number of received characters
+
+    \param[in]  inNumberOfCharacters    The number of characters to skip
+*/
+void usart_dma_rx_skip(size_t inNumberOfCharacters);
+
+
+/*!
+    Send Data on USART using DMA
+
+    This function blocks until all data has been processed
+
+    \param[in]  inBuffer    Buffer to send
+    \param[in]  inNumBytes  Number of bytes to send
+
+    \retval The number of bytes actually sent
+*/
 size_t usart_dma_write(uint8_t * inBuffer, size_t inNumBytes);
-void   usart_dma_rx_clear(void);
-void   usart_dma_rx_wait(void);
+
+/*!
+    Flush all received characters
+*/
+void usart_dma_rx_clear(void);
+
+/*!
+    Wait until a character arrived
+*/
+void usart_dma_rx_wait(void);
 
 
 #endif /* UART_DMA_H_ */
