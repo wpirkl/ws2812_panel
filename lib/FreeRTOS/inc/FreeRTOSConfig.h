7ab5156a7b88e307268cef2104c34e7232ec7ed8
wpirkl <werner.pirkl@gmail.com>
2015-10-15 10:46:36 +0200

    increase heap try putting heap to ccm

--- lib/FreeRTOS/inc/FreeRTOSConfig.h
+++ lib/FreeRTOS/inc/FreeRTOSConfig.h
@@ -94,7 +94,8 @@ extern uint32_t SystemCoreClock;
 #define configTICK_RATE_HZ						( ( TickType_t ) 1000 )
 #define configMAX_PRIORITIES					( 5 )
 #define configMINIMAL_STACK_SIZE				( ( unsigned short ) 128 )
-#define configTOTAL_HEAP_SIZE					( ( size_t ) ( 48 * 1024 ) )
+#define configTOTAL_HEAP_SIZE					( ( size_t ) ( 56 * 1024 ) )
+#define configAPPLICATION_ALLOCATED_HEAP		0
 #define configMAX_TASK_NAME_LEN					( 16 )
 #define configUSE_16_BIT_TICKS					0
 #define configIDLE_SHOULD_YIELD					1
